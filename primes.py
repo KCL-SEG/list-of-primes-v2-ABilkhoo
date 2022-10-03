2a5e1932c9e3c464db5758958b9bdae52bfc61e3
Bilkhoo, Aman (k21013503) <aman.bilkhoo@kcl.ac.uk>
2022-10-03 10:44:37 +0100

    Complete exercise

--- primes.py
+++ primes.py
@@ -2,5 +2,24 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if type(number_of_primes) != int:
+        raise TypeError("number_of_primes must be an int")
+    if number_of_primes <= 0:
+        raise ValueError("number_of_primes must be positive")
+
+
     list = []
+    
+    i = 2
+    while len(list) < number_of_primes:
+        prime = True;
+        for item in list:
+            if i % item == 0:
+                prime = False;
+                break;
+        
+        if prime == True:
+            list.append(i)
+        i += 1
+
     return list
